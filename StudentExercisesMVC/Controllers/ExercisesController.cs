94af719fac151014e50a77a1a4948579d8cc560e
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-11-13 13:17:56 -0600

    finished ExercisesController

---
+++ StudentExercisesMVC/Controllers/ExercisesController.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using StudentExercises;
+using StudentExercisesMVC.Models;
+using StudentExercises.Models.ViewModels;
+
+
+namespace StudentExercisesMVC.Controllers
+{
+    public class ExercisesController : Controller
+    {
+        private readonly IConfiguration _config;
+        public ExercisesController(IConfiguration config)
+        {
+            _config = config;
+        }
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // GET: Exercises
+        public ActionResult Index()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, ExerciseName, ProgrammingLanguage
+                                                            FROM Exercise";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Exercise> exercises = new List<Exercise>();
+                    while (reader.Read())
+                    {
+                        Exercise exercise = new Exercise()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            ExerciseName = reader.GetString(reader.GetOrdinal("ExerciseName")),
+                            ProgrammingLanguage = reader.GetString(reader.GetOrdinal("ProgrammingLanguage"))
+                        };
+
+                        exercises.Add(exercise);
+                    }
+                    reader.Close();
+                    return View(exercises);
+                }
+            }
+        }
+
+        // GET: Exercises/Details/5
+        public ActionResult Details(int id)
+        {
+            var exercise = GetById(id);
+            return View(exercise);
+        }
+
+        // GET: Exercises/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Exercises/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Exercise exercise)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"INSERT INTO Exercise VALUES (@ExerciseName, @ProgrammingLanguage)";
+                        cmd.Parameters.Add(new SqlParameter("@ExerciseName", exercise.ExerciseName));
+                        cmd.Parameters.Add(new SqlParameter("@ProgrammingLanguage", exercise.ProgrammingLanguage));
+                        cmd.ExecuteNonQuery();
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Exercises/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var exercise = GetById(id);
+            return View(exercise);
+        }
+
+        // POST: Exercises/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Exercise exercise)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"UPDATE Exercise 
+                            SET ExerciseName = @ExerciseName, ProgrammingLanguage = @ProgrammingLanguage
+                                    WHERE Id = @Id";
+                        cmd.Parameters.Add(new SqlParameter("@ExerciseName", exercise.ExerciseName));
+                        cmd.Parameters.Add(new SqlParameter("@ProgrammingLanguage", exercise.ProgrammingLanguage));
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        SqlDataReader reader = cmd.ExecuteReader();
+
+                        Exercise theExercise = null;
+                        if (reader.Read())
+                        {
+                            theExercise = new Exercise
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                ExerciseName = reader.GetString(reader.GetOrdinal("ExerciseName")),
+                                ProgrammingLanguage = reader.GetString(reader.GetOrdinal("ProgrammingLanguage"))
+                            };
+                        }
+
+                        reader.Close();
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Exercises/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var exercise = GetById(id);
+            return View(exercise);
+        }
+
+        // POST: Exercises/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"DELETE FROM StudentExercise WHERE ExerciseId = @id;
+                                                                DELETE FROM Exercise WHERE Id = @id";
+
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return RedirectToAction(nameof(Index));
+                        }
+                        throw new Exception("No rows affected.");
+                    }
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        private Exercise GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @" SELECT Id, ExerciseName, ProgrammingLanguage
+                                                            FROM Exercise
+                                                            WHERE id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Exercise theExercise = null;
+                    if (reader.Read())
+                    {
+                        theExercise = new Exercise
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            ExerciseName = reader.GetString(reader.GetOrdinal("ExerciseName")),
+                            ProgrammingLanguage = reader.GetString(reader.GetOrdinal("ProgrammingLanguage"))
+                        };
+                    }
+
+                    reader.Close();
+                    return theExercise;
+                }
+            }
+        }
+    }
+}
